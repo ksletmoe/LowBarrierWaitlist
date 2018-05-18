904956d5b8c07bea78f6558d8dbea5ec20bedf61
ksletmoe <kyle.sletmoe@gmail.com>
2018-05-18 15:47:12 -0700

    add administrator persistence

--- low_barrier_waitlist/persistence.py
+++ low_barrier_waitlist/persistence.py
@@ -1,8 +1,8 @@
 from . import mongo
-from .models import Participant
+from .models import Participant, Administrator
 
 
-def getParticipant(hmisid):
+def get_participant(hmisid):
     record = mongo.db.users.find_one({'hmis': str(hmisid)})
     if record:
         return Participant.load(record)
@@ -10,7 +10,22 @@ def getParticipant(hmisid):
         return None
 
 
-def updateParticipant(participant):
+def update_participant(participant):
     res = mongo.db.users.update_one({'hmis': str(participant.hmis)},
-        {'$set': participant.dump()})
+                                    {'$set': participant.dump()})
+    return res.modified_count == 1
+
+
+def get_administrator(email):
+    record = mongo.db.administrators.find_one({'email': email})
+    if record:
+        return Administrator.load(record)
+    else:
+        return None
+
+
+def update_administrator(administrator):
+    res = mongo.db.administrators.update_one({'email': administrator.email},
+                                             {'$set': administrator.dump()})
+
     return res.modified_count == 1
